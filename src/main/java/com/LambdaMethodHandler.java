8f27ee4d5e6e06a4a0aa5ba38c4a644f4016d8e8
Marcin <marcinkulik@icloud.com>
2019-10-25 17:31:13 +0100

    first commit

---
+++ src/main/java/com/LambdaMethodHandler.java
@@ -0,0 +1,11 @@
+package com;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+    public class LambdaMethodHandler implements RequestHandler<Request, Response> {
+        public Response handleRequest(Request request, Context context) {
+            String greetingString = String.format("Hello %s %s.", request.firstName, request.lastName);
+            return new Response(greetingString);
+        }
+    }
